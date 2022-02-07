e957e45bb764c631b05be30ffd035629afb7029c
Anupam Arya <anu9am@gmail.com>
2022-02-07 22:42:51 +0530

    Product Router to Add

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
+const productRoute = require('./routes/product');
 
 mongoose.connect(MONGO_URI).then(console.log("DB Connected")).catch((err) => (console.error(err)));
 app.use(express.json());
 
 app.use('/api/user', userRoute);
 app.use('/api/auth', authRoute);
+app.use('/api/product', productRoute);
 
 app.listen(PORT, () => { console.log("Backend server is running on: " +PORT); });
