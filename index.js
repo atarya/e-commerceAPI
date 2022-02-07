b6bc0691952a2bfcf18b24ab5fd363b67721eef7
Anupam Arya <anu9am@gmail.com>
2022-02-08 00:04:11 +0530

    Cart CRUD with JWT

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const MONGO_URI = process.env.MONGO_URI;
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product');
+const cartRoute = require('./routes/cart');
 
 mongoose.connect(MONGO_URI).then(console.log("DB Connected")).catch((err) => (console.error(err)));
 app.use(express.json());
@@ -14,5 +15,6 @@ app.use(express.json());
 app.use('/api/user', userRoute);
 app.use('/api/auth', authRoute);
 app.use('/api/product', productRoute);
+app.use('/api/cart', cartRoute);
 
 app.listen(PORT, () => { console.log("Backend server is running on: " +PORT); });
